ab34eb09b19597c60438801225f96ff9e045e32c
root <root@vm-VirtualBox.(none)>
2015-05-04 13:53:03 -0400

    update js for login

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
-dsjf
+$(document).ready(function (){
+	$("#loginButton").click(function(){
+		alert("login button");
+		username = $("#email").val();
+		getNonce(username);
+		
+		});
+	});
+});
+
+function getNonce(username){
+	$.ajax({
+		type: "POST",
+		url: "../php/verification.php"
+		data: {"username": username},
+		dataType: "json",
+		success: function(responseText){
+			alert(responseText);
+		}
+}
