81b6e0563a79e45cb7fcf1027f7173e4eec0f5cf
asbhandari88 <asb634@nyu.edu>
2015-05-04 15:22:21 -0400

    add sha256 hashing

---
+++ php/naunce_gen.php
@@ -0,0 +1,23 @@
+<?php
+	//include 'connection.php';
+
+	$username =  $_POST['username'];
+	$num = rand(10000,99999);
+	$token = $username."~".$num;
+
+
+	$sql_select = "SELECT id, user, pass FROM user";
+	$result_select = mysqli_query($conn, $sql_select);
+	$row = mysqli_fetch_assoc($result_select);
+	$pass = $row["pass"];
+	$id_user = $row["id"];
+	echo $id_user;
+	/*
+	$str = $username.$token.$pass;
+	$hash = ("sha256", $str);
+
+	$sql_insert = "INSERT INTO requests ("id_user, token, hash")
+	VALUES (".$id_user.", ".$token.", ".$hash.")";
+	$result_insert = mysqli_query($conn, $sql_insert);
+	*/
+?> 
